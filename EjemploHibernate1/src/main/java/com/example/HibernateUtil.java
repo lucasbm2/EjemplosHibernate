c2a32b85ea1002b0ee7b84d6cae86d4b95f1ef83
José Manuel Brazález Segovia <jmbs03@iesvirreymorcillo.es>
2025-01-17 12:31:30 +0100

    Se añaden ejemplo 1 y 2
    Se modifican los nombres de ejemplo para introducir uno más básico.

--- EjemploHibernate1/src/main/java/com/example/HibernateUtil.java
+++ EjemploHibernate1/src/main/java/com/example/HibernateUtil.java
@@ -7,49 +7,36 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
-    // El registro de servicios estándar de Hibernate, que gestiona la configuración
-    // y los servicios necesarios para la conexión.
     private static StandardServiceRegistry registry;
-
-    // La fábrica de sesiones, que es responsable de crear y gestionar las sesiones
-    // de Hibernate.
     private static SessionFactory sessionFactory;
 
-    // Metodo que devuelve la instancia de SessionFactory, creándola si aún no existe.
     public static SessionFactory getSessionFactory() {
-
-        if (sessionFactory == null) { // Verifica si sessionFactory ya está inicializado.
+        if (sessionFactory == null) {
             try {
-                // Construye el registro de servicios estándar a partir del archivo de
-                // configuración (hibernate.cfg.xml).
+                // Create registry
                 registry = new StandardServiceRegistryBuilder().configure().build();
 
-                // Fuentes de metadatos, que recopilan información de las entidades
-                // mapeadas y otras configuraciones.
+                // Create MetadataSources
                 MetadataSources sources = new MetadataSources(registry);
+//                sources.addAnnotatedClass(Employee.class);
 
-                // Construye los metadatos necesarios para configurar y crear la fábrica
-                // de sesiones.
+                // Create Metadata
                 Metadata metadata = sources.getMetadataBuilder().build();
 
-                // Construye y asigna la fábrica de sesiones utilizando los metadatos
-                // configurados.
+                // Create SessionFactory
                 sessionFactory = metadata.getSessionFactoryBuilder().build();
+
 
             } catch (Exception e) {
                 e.printStackTrace();
-                System.out.println("Error al crear Session Factory " + e.getMessage());
-
-                // Si ocurre un error, destruye el registro de servicios para liberar recursos.
                 if (registry != null) {
                     StandardServiceRegistryBuilder.destroy(registry);
                 }
             }
         }
-        return sessionFactory; // Devuelve la instancia de SessionFactory.
+        return sessionFactory;
     }
 
-    // Metodo para cerrar el registro de servicios y liberar los recursos asociados.
     public static void shutdown() {
         if (registry != null) {
             StandardServiceRegistryBuilder.destroy(registry);
